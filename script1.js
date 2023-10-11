713302ce6b2c37d94be15313eff2ecfa8a7210b8
SRIRAMOJUSRAVANTHI <147321410+SRIRAMOJUSRAVANTHI@users.noreply.github.com>
2023-10-11 21:05:07 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,81 @@
+
+    let bool= true;
+    let bool1= false;
+
+        let num1 = 30;
+
+        let num2 = 40;
+
+
+        console.log(num1>num2   &&  num1==40);//logical operators
+        console.log(num1==num2  ||  num1==50);
+        console.log(!num1<num2 && num1==50);
+
+
+        //functions
+
+        function chinni(){
+            console.log("hello");
+        }
+        function nani(){
+            console.log("hi");
+
+        }
+        function kav()
+        {
+            console.log("vanakam");
+
+        }
+
+        chinni();
+        nani();
+        kav();
+
+
+        
+        function addition(num1,num2)
+        {
+            console.log(num1+num2);
+        }
+        function subtraction(n1, n2)
+        {
+            console.log(n1-n2);
+        }
+        addition(2,4);
+        subtraction(4,2);
+
+        //string
+        let name ="chinnisravss";
+
+        console.log(name.length);
+        // for loop
+         for(let i=0;i<8;i++){
+
+            console.log("hello");
+
+         }
+for(let i=1;i<=20;i++)
+{
+    console.log("hii");
+}
+        
+for(let i=17;i>=1;i--)
+{
+    console.log("number"+i);
+}
+
+for(let i=1;i<=10;i++)
+{
+    console.log("number"+i);
+}
+for(let i=10;i>=1;i++)
+{
+    console.log("number"+i);
+}
+x=4+"4";
+document.write(x);
+
+
+
+
+    
